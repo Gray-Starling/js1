c67e446e8c7d9624493cc85bbcdabe0d2fffca3e
Gray Starling <skvorcovsa90@gmaul.com>
2021-02-21 16:42:42 +0300

    lesson01

---
+++ js/admin-name.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let admin;
+let name = "Василий";
+
+admin = name;
+
+alert(admin);
+
---
+++ js/celsius.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let celsius = 26;
+let fahrenheit;
+
+fahrenheit = (9 / 5) * celsius + 32;
+
+alert(fahrenheit.toFixed(2));
